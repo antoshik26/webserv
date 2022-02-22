c7d9b7611d2b163b9749178670c2acc5386a1bb0
Denisse Madeleine <dmadelei@mi-q3.msk.21-school.ru>
2022-02-22 17:46:33 +0300

    adsfg

--- cgi/cgi.cpp
+++ cgi/cgi.cpp
@@ -13,12 +13,12 @@ void cgi::new_cgi(std::string extension,std::map<std::string, std::map<std::stri
 //void cgi::new_cgi(std::strinf script,std::map<std::string, std::string> body) 
 {
 	fill_varibles(body);
-	find_script(extension,cgi); //comment 
+	// find_script(extension,cgi); //comment 
 	this->execve_script();
 	//this->execve_script(script);
 
-	delete _env[pos];
-	delete _env[pos+1];
+	// delete _env[pos];
+	// delete _env[pos+1];
 	//delete _script; depeends on args comment?
 }
 //void cgi::execve_script(std::string script)
@@ -67,11 +67,11 @@ cgi::~cgi()
 		return;
 	while(_env[i])
 	{
-		delete _env[i];
+		// delete _env[i];
 		i++;
 	}
-	delete[] _env[i+2];
-	delete [] _env;
+	// delete[] _env[i+2];
+	// delete [] _env;
 	//delete _script;
 }
 
